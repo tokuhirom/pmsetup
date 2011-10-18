44b4b73104303a24b66dc73c27c4d8501112e6cc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-19 08:14:07 +0900

    modernized package

---
+++ lib/pmsetup.pm
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+use utf8;
+
+package pmsetup;
+use v5.12;
+our $VERSION='0.01';
+
+1;
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+
+use_ok('pmsetup');
+
+done_testing;
+
